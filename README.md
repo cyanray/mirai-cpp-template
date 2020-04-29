889b481924cc79944ce71f0be28a42e9822bdfe1
Cyan <a484599279@hotmail.com>
2020-04-30 01:25:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# mirai-cpp-template
+
+ mirai-cpp 机器人的开发模板(CMake)。
+ 希望可以降低 C++ 新手入门编写 mirai 机器人的难度。
+
+## 使用方法
+
+使用 Visual Studio 或 Visual Studio Code 直接打开整个文件夹即可。
+
+本模板编译目标为可执行文件，其主函数位于 **src/main.cpp** 。
+
+要添加其他 **.cpp** 源文件，不要忘记修改 **CMakeLists.txt**。
+
+不熟悉 CMake 的话可以趁这个机会学习一下。
+
+mirai-cpp 的说明文档：[使用说明](https://github.com/cyanray/mirai-cpp/blob/master/doc/%E4%BD%BF%E7%94%A8%E8%AF%B4%E6%98%8E.md)
+
+## 编译出 Linux 可执行文件、部署到服务器上
+
+操作方法：
+
+1. 在 WSL 上编译出二进制可执行文件
+2. 把编译出来的二进制可执行文件复制到自己的服务器上
+
+如果你还不了解什么是 **WSL** (**W**indows **S**ubsystem for **L**inux) ，可以趁这个机会学习一下。
+
+使用 Visual Studio 的 CMake 项目可以很轻松的在 WSL 上进行编译。
+
+按照如图所示步骤，创建一个针对 WSL 平台的配置。因为我的 WSL 安装了 GCC 编译器，所以这里选择 **WSL-GCC-Releas**。
+
+![创建WSL-GCC平台配置1](./doc/pic/vs_3.png)
+
+![创建WSL-GCC平台配置2](./doc/pic/vs_configure_linux_project.png)
+
+之后将配置切换到刚刚创建的 WSL 平台配置，重新编译项目，编译出来的二进制文件就在 **[本模板所在的目录]/out/build/WSL-Release/** 目录下了(不出意外的话，它叫做 **my-qqrobot** )。
+
+把这个文件复制到你的 Linux 服务器上，可以直接运行哦！
---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+// 注意: 本项目的所有源文件都必须是 UTF-8 编码
+
+// 这是一个“反撤回”机器人
+// 在群里回复 “/anti-recall enabled.” 或者 “撤回没用” 之后
+// 如果有人在群里撤回，那么机器人会把撤回的内容再发出来
+
+#include <iostream>
+#include <mirai.h>
+#include "myheader.h"
+
+int main()
+{
+	using namespace std;
+	using namespace Cyan;
+
+#if defined(WIN32) || defined(_WIN32)
+	// 切换代码页，让 CMD 可以显示 UTF-8 字符
+	system("chcp 65001");
+#endif
+
+	MiraiBot bot("127.0.0.1", 8080);
+	while (true)
+	{
+		try
+		{
+			bot.Auth("InitKeyVl0CEUzZ", 211000000_qq);
+			break;
+		}
+		catch (const std::exception& ex)
+		{
+			cout << ex.what() << endl;
+		}
+		MiraiBot::SleepSeconds(1);
+	}
+	cout << "Bot Working..." << endl;
+
+	map<GID_t, bool> groups;
+
+	bot.On<GroupMessage>(
+		[&](GroupMessage m)
+		{
+			try
+			{
+				string plain = m.MessageChain.GetPlainText();
+				if (plain == "/anti-recall enabled." || plain == "撤回没用")
+				{
+					groups[m.Sender.Group.GID] = true;
+					m.Reply(MessageChain().Plain("撤回也没用，我都看到了"));
+					return;
+				}
+				if (plain == "/anti-recall disabled." || plain == "撤回有用")
+				{
+					groups[m.Sender.Group.GID] = false;
+					m.Reply(MessageChain().Plain("撤回有用"));
+					return;
+				}
+			}
+			catch (const std::exception& ex)
+			{
+				cout << ex.what() << endl;
+			}
+		});
+
+
+	bot.On<GroupRecallEvent>(
+		[&](GroupRecallEvent gm)
+		{
+			try
+			{
+				if (!groups[gm.Group.GID]) return;
+				auto recalled_mc = bot.GetGroupMessageFromId(gm.MessageId).MessageChain;
+				auto mc = "刚刚有人撤回了: " + recalled_mc;
+				bot.SendMessage(gm.Group.GID, mc);
+			}
+			catch (const std::exception& ex)
+			{
+				cout << ex.what() << endl;
+			}
+		});
+
+
+	bot.EventLoop();
+
+	return 0;
+}
