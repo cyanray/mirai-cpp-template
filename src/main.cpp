2ad855a4f1b9cf6e9fc9fa01e0a2a2f6132d36cd
cyanray <a484599279@hotmail.com>
2021-06-20 23:09:12 +0800

    更新.

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,31 @@
 #include <map>
 #include <mirai.h>
 #include "myheader.h"
+using namespace std;
+using namespace Cyan;
 
 int main()
 {
-	using namespace std;
-	using namespace Cyan;
-
 #if defined(WIN32) || defined(_WIN32)
 	// 切换代码页，让 CMD 可以显示 UTF-8 字符
 	system("chcp 65001");
 #endif
 
-	MiraiBot bot("127.0.0.1", 8080);
+	MiraiBot bot;
+	SessionOptions opts;
+	opts.BotQQ = 123456789_qq;				// 请修改为你的机器人QQ
+	opts.HttpHostname = "localhost";		// 请修改为和 mirai-api-http 配置文件一致
+	opts.WebSocketHostname = "localhost";	// 同上
+	opts.HttpPort = 8080;					// 同上
+	opts.WebSocketPort = 8080;				// 同上
+	opts.VerifyKey = "VerifyKey";			// 同上
+
 	while (true)
 	{
 		try
 		{
-			bot.Auth("mirai-api-http设置的AuthKey", 211000000_qq);
+			cout << "尝试与 mirai-api-http 建立连接..." << endl;
+			bot.Connect(opts);
 			break;
 		}
 		catch (const std::exception& ex)
@@ -80,8 +88,16 @@ int main()
 			}
 		});
 
-
-	bot.EventLoop();
+	string cmd;
+	while (cin >> cmd)
+	{
+		if (cmd == "exit")
+		{
+			// 程序结束前必须调用 Disconnect，否则 mirai-api-http 会内存泄漏。
+			bot.Disconnect();
+			break;
+		}
+	}
 
 	return 0;
 }
