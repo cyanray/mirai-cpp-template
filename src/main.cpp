4cc168cfa9011d248cea2e23953996fef6297e1e
cyanray <a484599279@hotmail.com>
2021-07-13 12:49:53 +0800

    update.

--- src/main.cpp
+++ src/main.cpp
@@ -88,6 +88,27 @@ int main()
 			}
 		});
 
+	// 在失去与mah的连接后重连
+	bot.On<LostConnection>([&](LostConnection e)
+		{
+			cout << e.ErrorMessage << " (" << e.Code << ")" << endl;
+			while (true)
+			{
+				try
+				{
+					cout << "尝试连接 mirai-api-http..." << endl;
+					bot.Reconnect();
+					cout << "与 mirai-api-http 重新建立连接!" << endl;
+					break;
+				}
+				catch (const std::exception& ex)
+				{
+					cout << ex.what() << endl;
+				}
+				MiraiBot::SleepSeconds(1);
+			}
+		});
+
 	string cmd;
 	while (cin >> cmd)
 	{
